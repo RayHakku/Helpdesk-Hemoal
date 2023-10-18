5f2e33ee59db6c992b579a3e203fa9361b6ef248
RayHakku <104810422+RayHakku@users.noreply.github>
2023-10-18 14:32:06 -0300

    feat: Update SecurityConfiguration.java
    This commit updates the SecurityConfiguration.java file with the following changes:
    
    1.Added the BCryptPasswordEncoder bean to provide password encoding.
    2.Made configuration changes for better security.

--- helpdesk/src/main/java/com/hemoal/helpdesk/security/SecurityConfiguration.java
+++ helpdesk/src/main/java/com/hemoal/helpdesk/security/SecurityConfiguration.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.hemoal.helpdesk.services.UserDetailsServicesImplement;
@@ -29,17 +32,23 @@ public class SecurityConfiguration {
     http
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(requests -> requests
-            .requestMatchers(HttpMethod.POST).permitAll()
+            .requestMatchers("/api/auth/**").permitAll()
             .anyRequest().authenticated())
             .httpBasic(withDefaults());
         return http.build();
         }
 
-    // @Bean
-    // public AuthenticationManager authenticationManagerBean() throws Exception {
-    //     return super.authenticationManagerBean();
-    // }
+    @Bean
+    public AuthenticationManager authenticationManagerBean(
+        AuthenticationConfiguration authenticationConfiguration
+      ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+      }
     
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
 
